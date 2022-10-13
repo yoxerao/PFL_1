fb37480d05341bc92fc99440e460f7cc47ffb94f
Sofia Moura <sofia.smoura@hotmail.com>
2022-10-13 20:24:51 +0100

    started normalization

--- proj.hs
+++ proj.hs
@@ -1,7 +1,17 @@
-type Polynomial = [(Integer,[Char],[Integer])] 
+{- type Polynomial = [(Integer,[Char],[Integer])] -}
+type Polynomial = [(Integer,[(Char, Integer)])]
+
+removeNullMonomials :: Polynomial -> Polynomial
+removeNullMonomials p = filter (\(n, [(var, exp)]) -> n /= 0) p
+
+{- removeNullExpVars :: Polynomial -> Polynomial
+removeNullExpVars p = filter (\(n, [(var, exp)]) -> exp /= 0) p -}
+
+{- normalPolynomial :: Polynomial -> Polynomial
+normalPolynomial [] = []
+normalPolynomial (x:xs) =  -}
 
 addPolynomial :: Polynomial -> Polynomial -> Polynomial
 addPolynomial [] ys = ys
 addPolynomial xs [] = xs
 addPolynomial xs ys = normalPolynomial xs:ys
-
