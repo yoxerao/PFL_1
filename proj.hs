6ce64341ac84c40bafa4b7d611f28e789e025fd8
Fábio Almeida Teixeira <up202006345@up.pt>
2022-10-14 00:19:39 +0100

    created 'stripPoly' and 'removeNullExp' functions

--- proj.hs
+++ proj.hs
@@ -1,17 +1,25 @@
 {- type Polynomial = [(Integer,[Char],[Integer])] -}
-type Polynomial = [(Integer,[(Char, Integer)])]
+type Polynomial = [(Int,[(Char, Integer)])]
 
-removeNullMonomials :: Polynomial -> Polynomial
-removeNullMonomials p = filter (\(n, [(var, exp)]) -> n /= 0) p
+{- removes all variables with a 0 exponent from a single monomial -}
+removeNullExp :: [(Char, Integer)] -> [(Char,Integer)]
+removeNullExp [] = []
+removeNullExp ((a,b):xs)
+    | b == 0 = removeNullExp xs
+    | otherwise = (a,b): removeNullExp xs
 
-{- removeNullExpVars :: Polynomial -> Polynomial
-removeNullExpVars p = filter (\(n, [(var, exp)]) -> exp /= 0) p -}
+{- removes all monomials whose coeficient is 0 and calls 'removeNullExp' on all monomials -}
+stripPoly :: Polynomial -> Polynomial
+stripPoly [] = []
+stripPoly ((0,x:xs):xss) = stripPoly xss
+stripPoly ((a,[]):xs) = (a,[]):stripPoly xs
+stripPoly ((a,(b, c):xs):xss) = (a, removeNullExp ((b,c):xs) ): stripPoly xss
 
 {- normalPolynomial :: Polynomial -> Polynomial
 normalPolynomial [] = []
 normalPolynomial (x:xs) =  -}
 
-addPolynomial :: Polynomial -> Polynomial -> Polynomial
+{-addPolynomial :: Polynomial -> Polynomial -> Polynomial
 addPolynomial [] ys = ys
 addPolynomial xs [] = xs
-addPolynomial xs ys = normalPolynomial xs:ys
+addPolynomial xs ys = normalPolynomial xs:ys -}
